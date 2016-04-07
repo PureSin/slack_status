03188611c1e95a323ba2a3678e1ded74afea4168
Kelvin Ma <kelvin@codecademy.com>
2016-04-06 20:52:24 -0700

    sets slack status

--- status.go
+++ status.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/nlopes/slack"
+	"os"
 )
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	api := slack.New(os.Getenv("SLACK_API_TOKEN"))
+	// api.SetDebug(true)
+	if len(os.Args) < 2 {
+		fmt.Println("Need status arg: auto|away")
+		return
+	}
+	status := os.Args[1]
+	err := api.SetUserPresence(status)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
 }
