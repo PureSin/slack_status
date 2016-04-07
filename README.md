530269e8a29ee99594f41fcf7419baea2e34d3a5
Kelvin Ma <kelvin@codecademy.com>
2016-04-06 20:31:33 -0700

    setup repo

---
+++ README.md
@@ -0,0 +1,3 @@
+sets slack status on OS X when screen locks/unlocks
+
+Needs sleep watcher: http://www.bernhard-baehr.de/
---
+++ status.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/nlopes/slack"
+)
+
+func main() {
+	fmt.Printf("Hello, world.\n")
+}
